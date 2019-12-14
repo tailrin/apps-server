fb9a6608cff69e46f6996121d5a91ce634e95ce7
joshua hahn <tailrin.jh@gmail.com>
2019-12-14 12:32:06 -0700

    added nodemon

--- apps-server.js
+++ apps-server.js
@@ -1,2 +1,11 @@
 const playstore = require('./playstore');
-const express = require('express')
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express!');
+});
+
+app.listen(8000, () => {
+  console.log('Express server is listening on port 8000!');
+});
