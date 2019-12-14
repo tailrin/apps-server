4fe3932fb5a8453a9e312f05e8a5962214141c5e
joshua hahn <tailrin.jh@gmail.com>
2019-12-14 13:55:23 -0700

    Cleaned up formatting

--- apps-server.js
+++ apps-server.js
@@ -14,36 +14,36 @@ app.get('/apps', (req, res) => {
   }
 
   if(!!sort){
-      if(sort !== 'rating'  && sort !== 'app'){
-        res.status(400).send("Can only sort by rating or app")
-      }else if(sort === ''){
-        res.send(playstore)
-      }else if(sort === 'rating'){
-        const newArr = playstore.slice(0)
-        newArr.sort(function(a, b){return b.Rating - a.Rating});
-        res.send(newArr);
-      }else if(sort === 'app'){
-        const newArr = playstore.slice(0);
-        newArr.sort((a,b) => {
-          if ( a.App < b.App ){
-            return -1;
-          }
-          if ( a.App > b.App ){
-            return 1;
-          }
-          return 0;
-        });
-        res.send(newArr)
-      }
+    if(sort !== 'rating'  && sort !== 'app'){
+      res.status(400).send("Can only sort by rating or app")
+    }else if(sort === ''){
+      res.send(playstore)
+    }else if(sort === 'rating'){
+      const newArr = playstore.slice(0)
+      newArr.sort(function(a, b){return b.Rating - a.Rating});
+      res.send(newArr);
+    }else if(sort === 'app'){
+      const newArr = playstore.slice(0);
+      newArr.sort((a,b) => {
+        if ( a.App < b.App ){
+          return -1;
+        }
+        if ( a.App > b.App ){
+          return 1;
+        }
+        return 0;
+      });
+      res.send(newArr);
+    }
   }
 
-    if(!!genres) {
-        if(!acceptableGenres.includes(genres)) {
-            res.status(400).send(`Be sure to choose one of the following ${acceptableGenres.join(', ')}.`);
-        }else {
-            res.send(playstore.filter(game =>  game.Genres.includes(genres)));
-        }
+  if(!!genres) {
+    if(!acceptableGenres.includes(genres)) {
+      res.status(400).send(`Be sure to choose one of the following ${acceptableGenres.join(', ')}.`);
+    }else {
+      res.send(playstore.filter(game =>  game.Genres.includes(genres)));
     }
+  }
 
 
   // res.send("this is a test")
