22d5b5531ef6a31ef779564cf983d363eac3258c
joshua hahn <tailrin.jh@gmail.com>
2019-12-17 21:29:17 -0700

    confirm up to date

--- test/apps-server.test.js
+++ test/apps-server.test.js
@@ -13,5 +13,25 @@ describe('Apps-server', () => {
                 let application = res.body[0];
                 expect(application).to.have.all.keys('App', 'Category', 'Rating', 'Reviews', 'Size', 'Installs', 'Type', 'Price', 'Content Rating', 'Genres', 'Last Updated', 'Current Ver', 'Android Ver');
             });
-    })
-})
+    });
+
+    it('should return return the array sorted by app', () => {
+        return request(app)
+            .get('/apps')
+            .query({sort: 'app'})
+            .expect(200)
+            .then( res =>{
+                expect(res.body).to.be.an('array');
+                expect(res.body).to.have.lengthOf.at.least(1);
+                let i = 0;
+                let sorted = true;
+                while(sorted && i < res.body.length -1){
+                    let name = res.body[i].App;
+                    let nextName = res.body[i + 1].App;
+                    sorted = sorted && name < nextName;
+                    i++
+                }
+                expect(sorted).to.be.true;
+            });
+    });
+});
